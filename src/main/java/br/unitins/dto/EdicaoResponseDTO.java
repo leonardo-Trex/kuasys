afd5daf9c775647aaa445ee7905fd422a85332e4
Leonardo <leonardosoares@unitins.br>
2026-04-02 10:58:26 -0300

    RequestDTO e ResponseDTO da classe Edição implementadas.

--- src/main/java/br/unitins/dto/EdicaoResponseDTO.java
+++ src/main/java/br/unitins/dto/EdicaoResponseDTO.java
@@ -1,19 +1,22 @@
 package br.unitins.dto;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import br.unitins.model.enums.GeneroQuadrinho;
-import br.unitins.model.enums.TipoCapa;
+import java.time.LocalDateTime;
+import br.unitins.model.Edicao;
 
 public record EdicaoResponseDTO(
-        Long id,
-        String nome,
-        Double preco,
-        Integer numero,
-        LocalDate dataPublicacao,
-        String isbn,
-        Integer tiragem,
-        TipoCapa tipoCapa,
-        String dimensoes,
-        GeneroQuadrinho genero) {
+                Long id,
+                String nome,
+                String descricao,
+                BigDecimal preco,
+                Integer numero,
+                LocalDate dataPublicacao,
+                String isbn,
+                Integer tiragem,
+                String tipoCapa, // Podemos retornar o label ou a String do Enum
+                String genero,
+                String dimensoes,
+                LocalDateTime dataCadastro) {
+
 }
--- src/main/java/br/unitins/resource/EdicaoResource.java
+++ src/main/java/br/unitins/resource/EdicaoResource.java
@@ -3,14 +3,22 @@ package br.unitins.resource;
 import java.util.List;
 
 import br.unitins.Service.interfaces.EdicaoService;
+import br.unitins.dto.EdicaoRequestDTO;
+import br.unitins.dto.EdicaoResponseDTO;
+import br.unitins.mapper.EdicaoMapper;
 import br.unitins.model.Edicao;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/edicoes")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,6 +34,43 @@ public class EdicaoResource {
                 .stream()
                 .map(EdicaoMapper::toResponseDTO)
                 .toList();
+
         return Response.ok(lista).build();
+    }
+
+    // public Response buscarPeloNome() {
+
+    // }
+
+    @GET
+    @Path("/{id}")
+    public Response buscarPeloId(Long id) {
+        return Response.ok(EdicaoMapper.toResponseDTO(service.findById(id))).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deletar(Long id) {
+        service.delete(id);
+
+        return Response.noContent().build();
+    }
+
+    @POST
+    public Response incluir(@Valid EdicaoRequestDTO dto) {
+        Edicao edicao = service.create(EdicaoMapper.toEntity(dto));
+
+        return Response
+                .status(Status.CREATED)
+                .entity(EdicaoMapper.toResponseDTO(edicao))
+                .build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response alterar(Long id, EdicaoRequestDTO dto) {
+        service.update(id, EdicaoMapper.toEntity(dto));
+
+        return Response.ok().build();
     }
 }
