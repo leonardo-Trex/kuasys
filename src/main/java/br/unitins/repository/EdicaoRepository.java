6e86c60056c39024444913def049353da1db3bd0
Leonardo <leonardosoares@unitins.br>
2026-04-02 10:56:54 -0300

    Busca pelo nome implementada.

--- src/main/java/br/unitins/repository/EdicaoRepository.java
+++ src/main/java/br/unitins/repository/EdicaoRepository.java
@@ -1,10 +1,14 @@
 package br.unitins.repository;
 
 import br.unitins.model.Edicao;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class EdicaoRepository implements PanacheRepository<Edicao> {
 
+    public PanacheQuery<Edicao> findByNome(String nome) {
+        return find("SELECT e FROM tb_edicao e WHERE UPPER(e.nome) LIKE UPPER(?1)", "%" + nome + "%");
+    }
 }
