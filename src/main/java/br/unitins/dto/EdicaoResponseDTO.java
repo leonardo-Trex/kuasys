d6bf329a787becb372bd82cc6293a7f3340cd946
Leonardo <leonardosoares@unitins.br>
2026-04-02 11:22:37 -0300

    pequena limpeza nos imports não utilizados.

--- src/main/java/br/unitins/dto/EdicaoResponseDTO.java
+++ src/main/java/br/unitins/dto/EdicaoResponseDTO.java
@@ -3,20 +3,19 @@ package br.unitins.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import br.unitins.model.Edicao;
 
 public record EdicaoResponseDTO(
-                Long id,
-                String nome,
-                String descricao,
-                BigDecimal preco,
-                Integer numero,
-                LocalDate dataPublicacao,
-                String isbn,
-                Integer tiragem,
-                String tipoCapa, // Podemos retornar o label ou a String do Enum
-                String genero,
-                String dimensoes,
-                LocalDateTime dataCadastro) {
+        Long id,
+        String nome,
+        String descricao,
+        BigDecimal preco,
+        Integer numero,
+        LocalDate dataPublicacao,
+        String isbn,
+        Integer tiragem,
+        String tipoCapa,
+        String genero,
+        String dimensoes,
+        LocalDateTime dataCadastro) {
 
 }
