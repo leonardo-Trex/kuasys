903866720245cc09fb13950c36cc9d8ceccd5628
Leonardo <leonardosoares@unitins.br>
2026-04-09 20:26:05 -0300

    pequena correção no repository de pessoa.

--- src/main/java/br/unitins/repository/PessoaRepository.java
+++ src/main/java/br/unitins/repository/PessoaRepository.java
@@ -3,7 +3,9 @@ package br.unitins.repository;
 import br.unitins.model.Pessoa;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class PessoaRepository implements PanacheRepository<Pessoa> {
 
     public PanacheQuery<Pessoa> findByNome(String nome) {
