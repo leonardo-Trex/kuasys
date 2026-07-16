9a1d7d7c484b2733e263ef6fe41cc54cabe590ab
Leonardo <leonardosoares@unitins.br>
2026-07-16 10:47:45 -0300

    src/.../dto/pedido/ Pequena limpeza
    - Classe enxuta para melhor entendimento

--- src/main/java/br/unitins/dto/itempedido/ItemPedidoCreateDTO.java
+++ src/main/java/br/unitins/dto/itempedido/ItemPedidoCreateDTO.java
@@ -1,13 +1,7 @@
 package br.unitins.dto.itempedido;
 
-import java.math.BigDecimal;
-
 public record ItemPedidoCreateDTO(
-        Long id,
         Long edicaoId,
-        String nome,
-        int quantidade,
-        BigDecimal precoUnitario
+        Integer quantidade
 ) {
-
 }
